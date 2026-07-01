f4c72d013ca7e61497f4f8b5bd20ce2f16b94db1
Matthew-Adjarko <attapanyin206@gmail.com>
2026-07-01 15:22:40 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const button = document.querySelector(".download-btn");
+
+button.addEventListener("click", () => {
+
+    alert("Thank you for downloading 'Do I Know You? Then I Loved You'. Enjoy reading!");
+
+});
